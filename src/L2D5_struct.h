29182d456b33e41c131d511cc961058096087494
kaliassistant <1325077@gmail.com>
2025-04-28 21:03:42 +0800

    Update L2.5 struct

--- src/L2D5_struct.h
+++ src/L2D5_struct.h
@@ -14,6 +14,7 @@
  *   - Any changes must be approved and reviewed carefully.
  *   - Modifying the structure will break compatibility between devices.
  *   - Always ensure sizeof(L2D5Frame) == 216 bytes.
+ *   - Always ensure sizeof(L2D5Frame_Encrypted) == 216 bytes.
  *
  */
 
@@ -67,8 +68,107 @@ extern "C" {
  *  *F: FLAG  
  */
 
+/*
+ *  L2.5 TAG definition
+ *  
+ *  |======================================== TAG ========================================|
+ *  ---------------------------------------------------------------------------------------
+ *  | NAME | ENCRYPTED | OEMR | FORWARD | TCP | BOARDCAST | ANYCAST | MULTICAST | UNICAST |
+ *  ---------------------------------------------------------------------------------------
+ *  | bit  |     0     |   1  |     2   |  3  |     4     |    5    |     6     |     7   |
+ *
+ *  TYPE definition
+ *
+ *  |     TYPE/bit     | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+ *  ----------------------------------------------------
+ *  |   HELLO_PKT_NB   | 0 | 1 | 0 | 0 | 1 | 0 | 0 | 0 |
+ *  ----------------------------------------------------
+ *  |   HELLO_PKT_RM   | 1 | 1 | 1 | 0 | 0 | 0 | 1 | 1 |  // TODO : MULTICAST OR UNICAST? (UNICAST, but MULTICAST...)
+ *  ----------------------------------------------------
+ *  |   TCP_DATA_DC    | 1 | 0 | 0 | 1 | 0 | 0 | 0 | 1 |
+ *  ----------------------------------------------------
+ *  |   TCP_DATA_RM    | 1 | 0 | 1 | 1 | 0 | 0 | 0 | 1 |
+ */
 
 
+/*
+ *  L2.5 FLAG definition
+ *
+ *  |====================== FLAG ======================
+ *  ---------------------------------------------------
+ *  | NAME |       PRI       |  ERR FLAG  | NULL FLAG |
+ *  ---------------------------------------------------
+ *  | bit  | 0-3 (0-15, L->H)|  4-5 *U1*  | 6-7 (N/A) |
+ *
+ *  *U1*: ERR FLAG definition
+ *  
+ *  | TYPE/bit | 4 | 5 |
+ *  --------------------
+ *  | ERR_NML  | 0 | 0 |
+ *  --------------------
+ *  | ERR_DBG  | 0 | 1 |
+ *  --------------------
+ *  | ERR_ERR  | 1 | 0 |
+ *  --------------------
+ *  | ERR_WARN | 1 | 1 |
+ */
+
+#define MAGIC_L2D5LAYER_TAGTYPE_HELLO_PKT_NB 0b01001000
+#define MAGIC_L2D5LAYER_TAGTYPE_HELLO_PKT_RM 0b11100011
+#define MAGIC_L2D5LAYER_TAGTYPE_TCP_DATA_DC 0b10010001
+#define MAGIC_L2D5LAYER_TAGTYPE_TCP_DATA_RM 0b10110001
+
+typedef enum {
+  L2D5TAG_HELLO_PKT_NB = 0b01001000,
+  L2D5TAG_HELLO_PKT_RM = 0b11100011,
+  L2D5TAG_TCP_DATA_DC = 0b10010001,
+  L2D5TAG_TCP_DATA_RM = 0b10110001
+} L2D5TAG_t;
+
+typedef enum {
+  L2D5FLAG_ERR_NML  = 0b00,
+  L2D5FLAG_ERR_DBG  = 0b01,
+  L2D5FLAG_ERR_ERR  = 0b10,
+  L2D5FLAG_ERR_WARN = 0b11
+} L2D5FLAG_ERR_t;
+
+typedef enum {
+  L2D5FLAG_NUL_A = 0b00,
+  L2D5FLAG_NUL_B = 0b01,
+  L2D5FLAG_NUL_C = 0b10,
+  L2D5FLAG_NUL_D = 0b11
+} L2D5FLAG_NUL_t;
+
+
+#define L2D5FLAG_PRI_SHIFT 0
+#define L2D5FLAG_ERR_SHIFT 4
+#define L2D5FLAG_NUL_SHIFT 6
+
+#define L2D5FLAG_PRI_MASK  (0x0F << L2D5FLAG_PRI_SHIFT) // bits 0-3
+#define L2D5FLAG_ERR_MASK  (0x03 << L2D5FLAG_ERR_SHIFT) // bits 4-5
+#define L2D5FLAG_NUL_MASK  (0x03 << L2D5FLAG_NUL_SHIFT) // bits 6-7
+                                                        
+                                                        
+static inline uint8_t L2D5FLAG_MKFLAG(uint8_t _pri, L2D5FLAG_ERR_t _err_flag, L2D5FLAG_NUL_t _nul_flag) {
+  return ((_pri & 0x0F) << L2D5FLAG_PRI_SHIFT) |
+         ((_err_flag & 0x03) << L2D5FLAG_ERR_SHIFT) |
+         ((_nul_flag & 0x03) << L2D5FLAG_NUL_SHIFT);
+}
+
+static inline uint8_t L2D5FLAG_GET_PRI(uint8_t _aByte) {
+  return (_aByte & L2D5FLAG_PRI_MASK) >> L2D5FLAG_PRI_SHIFT;
+}
+
+static inline L2D5FLAG_ERR_t L2D5FLAG_GET_ERR(uint8_t _aByte) {
+  return (L2D5FLAG_ERR_t)((_aByte & L2D5FLAG_ERR_MASK) >> L2D5FLAG_ERR_SHIFT);
+}
+
+static inline L2D5FLAG_NUL_t L2D5FLAG_GET_NUL(uint8_t _aByte) {
+  return (L2D5FLAG_NUL_t)((_aByte & L2D5FLAG_NUL_MASK) >> L2D5FLAG_NUL_SHIFT);
+}
+
+
+/* L2.5 Frame structure definition (must be 216 bytes) */
 typedef struct {
   /* 216 Bytes */
   uint8_t TAG;                        // L2.5: 0x00
@@ -80,6 +180,19 @@ typedef struct {
   uint8_t Payload[172];               // L2.5: 0x2C-0xD7
 } L2D5Frame;
 
+
+/* L2.5 Frame Encrypted structure definition (must be 216 bytes) */
+typedef struct {
+  /* 216 Bytes */
+  uint8_t TAG;                        // L2.5: 0x00
+  uint8_t KeyHint[8];                 // L2.5: 0x01-0x08
+  uint8_t FLAG;                       // L2.5: 0x09
+  uint8_t EncryptedPayload[206];      // L2.5: 0x0A-0xD7
+} L2D5Frame_Encrypted;
+
+
+STATIC_ASSERT(sizeof(L2D5Frame) == 216, L2D5Frame_must_be_216bytes);
+STATIC_ASSERT(sizeof(L2D5Frame_Encrypted) == 216, L2D5Frame_Encrypted_must_be_216bytes);
 
 #ifdef __cplusplus
 }
